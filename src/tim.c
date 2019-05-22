223a6c05771cab22e14b6802d5156e83024f0c16
mkusaka <hinoshita1992@gmail.com>
2019-05-23 01:28:46 +0900

    :+1: feat: apply generated ch3 tokenizer to main function

--- src/tim.c
+++ src/tim.c
@@ -209,40 +209,16 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  user_input = argv[1];
-  tokenize();
+  tokenize(argv[1]);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  if (tokens[0].ty != TK_NUM)
-    error_at(tokens[0].input, "数ではありません");
-  printf("  mov rax, %d\n", tokens[0].val);
+  gen(node);
 
-  int i = 1;
-
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        error_at(tokens[i].input, "数ではありません");
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        error_at(tokens[i].input, "数ではありません");
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
