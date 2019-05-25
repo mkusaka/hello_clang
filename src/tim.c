697ae01633393e86473a12843abccbb3635fd322
mkusaka <hinoshita1992@gmail.com>
2019-05-25 18:42:06 +0900

    :bug: fix: use == 0 check for strncmp

--- src/tim.c
+++ src/tim.c
@@ -61,28 +61,28 @@ void tokenize(char *user_input) {
       continue;
     }
 
-    if (strncmp(p, "==", 2)) {
+    if (strncmp(p, "==", 2) == 0) {
       tokens[i].ty = TK_EQ;
       tokens[i].input = p;
       i++;
       p += 2;
     }
 
-    if (strncmp(p, "!=", 2)) {
+    if (strncmp(p, "!=", 2) == 0) {
       tokens[i].ty = TK_NE;
       tokens[i].input = p;
       i++;
       p += 2;
     }
 
-    if (strncmp(p, "<=", 2)) {
+    if (strncmp(p, "<=", 2) == 0) {
       tokens[i].ty = TK_LE;
       tokens[i].input = p;
       i++;
       p += 2;
     }
 
-    if (strncmp(p, ">=", 2)) {
+    if (strncmp(p, ">=", 2) == 0) {
       tokens[i].ty = TK_GE;
       tokens[i].input = p;
       i++;
