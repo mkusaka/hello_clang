8f3eef74ecd5a6f7849cbcdf0be58402ad08e0aa
mkusaka <hinoshita1992@gmail.com>
2019-06-08 16:37:59 +0900

    :green_heart: test: add test for Vector

--- src/tim.c
+++ src/tim.c
@@ -50,6 +50,53 @@ typedef struct {
 // 100しかトークナイズできないのでそれ以上長いと扱えない
 Token tokens[100];
 
+// 可変長ベクタ
+typedef struct {
+  void **data;
+  int capacity;
+  int len;
+} Vector;
+
+Vector *new_vector() {
+  Vector *vec = malloc(sizeof(Vector));
+  vec->data = malloc(sizeof(void *) * 16);
+  vec->capacity = 16;
+  vec->len = 0;
+  return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+  if (vec->capacity == vec->len) {
+    vec->capacity *= 2;
+    vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+  }
+  vec->data[vec->len++] = elem;
+}
+
+// test for vector
+
+void expect(int line, int expected, int actual) {
+  if (expected == actual)
+    return;
+  fprintf(stderr, "%d: %d expected, bug got %d\n", line, expected, actual);
+  exit(1);
+}
+
+void runtest() {
+  Vector *vec = new_vector();
+  expect(__LINE__, 0, vec->len);
+
+  for (int i = 0; i < 100; i++)
+    vec_push(vec, (void *)i);
+
+  expect(__LINE__, 100, vec->len);
+  expect(__LINE__, 0, (long)vec->data[0]);
+  expect(__LINE__, 50, (long)vec->data[50]);
+  expect(__LINE__, 99, (long)vec->data[99]);
+
+  printf("Ok\n");
+}
+
 void tokenize(char *user_input) {
   char *p = user_input;
 
@@ -330,6 +377,11 @@ int main(int argc, char **argv) {
   if (argc !=2) {
     fprintf(stderr, "引数の個数が正しくありません。");
     return 1;
+  }
+
+  if (strncmp(argv[1], "-test", 5) == 0) {
+    runtest();
+    return 0;
   }
 
   tokenize(argv[1]);
